9e062698b4ddfd1937c16dc6ce2a453d40a3ff7b
MeatisX10 <meatis.b@extend-os.com>
2024-10-25 16:04:51 +0700

    FATCA&AML UAT
    1. AML - assertion by page before execute
    2. FATCA - edit endpoint to be UAT
    
    Co-Authored-By: MeatisX10 <185473269+meatisx10@users.noreply.github.com>

--- AML-Webview-UAT.cy.js
+++ AML-Webview-UAT.cy.js
@@ -36,7 +36,8 @@ describe('AML', () => {
         cy.get('.btn-confirm').click()
         cy.wait(6000)
         cy.get('.btn-confirm').scrollIntoView({ duration: 200 }).click()
-        cy.log('Working Infomation')
+
+        cy.get('.title').should('have.text', 'Working information')
         cy.get(':nth-child(1) > [style="font-size: 22px; height: 44px; border-radius: 4px; border: 1px solid rgb(201, 198, 205); padding: 0px 12px; display: flex; flex-direction: row; justify-content: space-between; align-items: center; color: rgb(53, 50, 57);"]').click()
         cy.wait(700)
         cy.get('.scroll-content > :nth-child(7)').click()
@@ -44,8 +45,10 @@ describe('AML', () => {
         cy.get(':nth-child(2) > [style="display: flex; flex-direction: column;"] > .textfield-input')
           .clear().type(`พรรคการเมืองแห่งหนึ่ง${rand}`)// Company
         cy.get('[style="display: flex; justify-content: center; align-items: center; width: 24px;"] > img').click()
+
+        cy.get('.title').should('have.text', 'Working address')
         cy.get('.infomation-content > :nth-child(1) > [style="display: flex; flex-direction: column;"] > .textfield-input')
-          .clear().type(`${Math.floor(Math.random()*100)}/${rand}`) // house no.
+          .clear().type(`${Math.floor(Math.random() * 100)}/${rand}`) // house no.
         cy.get('.infomation-content > :nth-child(2) > [style="display: flex; flex-direction: column;"] > .textfield-input')
           .clear().type(`คอนโดในหมู่บ้านชนบทในเมืองหลวงแห่งนึง${rand}`) //Village name/building
         cy.get('[style="background-color: white; margin-top: 16px; padding-right: 8px;"] > [style="display: flex; flex-direction: column;"] > .textfield-input')
@@ -59,35 +62,34 @@ describe('AML', () => {
         cy.get(':nth-child(6) > [style="display: flex; flex-direction: column;"] > .textfield-input')
           .clear().type(`โดดเดี่ยวในซอยเปลี่ยนใจกลางหมู่บ้าน${rand}`) //trok
         cy.get(':nth-child(7) > [style="display: flex; flex-direction: column;"] > .textfield-input')
-        .clear().type(`เส้นหลักของประเทศสารขัณฑ์${rand}`) //road
+          .clear().type(`เส้นหลักของประเทศสารขัณฑ์${rand}`) //road
         cy.log('Working Address')
         cy.get('[style="font-size: 22px; height: 44px; border-radius: 4px; border: 1px solid rgb(201, 198, 205); padding: 0px 12px; display: flex; flex-direction: row; justify-content: space-between; align-items: center; color: rgb(53, 50, 57);"]').click()
-        cy.get(`.scroll-content > :nth-child(${Math.floor(Math.random() * 77)+1}`).click()//Province
+        cy.wait(1000)
+        cy.get(`.scroll-content > :nth-child(${Math.floor(Math.random() * 77) + 1}`).click()//Province
         cy.wait(700)
         cy.get('.scroll-content > :nth-child(1)').click() // District
         cy.wait(700)
         cy.get('.scroll-content > :nth-child(1)').click() // Sub-District
         cy.wait(700)
         cy.get('[style="padding: 24px; background-color: rgb(255, 255, 255); border: 1px solid rgb(234, 231, 236);"]').click() // Postcode
-        cy.wait(700)
-        cy.get('.btn-confirm').scrollIntoView({ duration: 200 }).click()
-        cy.log('Back to Working Infomation')
-        cy.get('.scroll-content').scrollTo('bottom')
-        cy.wait(700)
-        cy.get(':nth-child(4) > [style="font-size: 22px; height: 44px; border-radius: 4px; border: 1px solid rgb(201, 198, 205); padding: 0px 12px; display: flex; flex-direction: row; justify-content: space-between; align-items: center; color: rgb(53, 50, 57);"] > img').click() // SourceofIncome
-        cy.wait(700)
-        cy.get(`.scroll-content > :nth-child(${Math.floor(Math.random() * 250)+1}`).click()
-        cy.get(':nth-child(5) > [style="display: flex; flex-direction: column;"] > .textfield-input').clear().type(`${rand}00`)
-        cy.wait(700)
-        cy.get('.btn-confirm').click()
-        cy.wait(700)
-        cy.log('Contact Infomation')
-        cy.get('.btn-confirm').click()
         cy.wait(1000)
-        cy.log('Review')
-        cy.get('[style="align-content: end; padding: 24px 16px; flex: 1 1 0%; background-color: rgb(245, 243, 246);"]').scrollIntoView({ duration: 2000 }) // Scrolls 'footer' into view
+        cy.get('.btn-confirm').scrollIntoView({ duration: 700 }).click()
+
+        cy.get('.title').should('have.text', 'Working information')
+        cy.get('.scroll-content').scrollTo('bottom')
+        cy.get(':nth-child(4) > [style="font-size: 22px; height: 44px; border-radius: 4px; border: 1px solid rgb(201, 198, 205); padding: 0px 12px; display: flex; flex-direction: row; justify-content: space-between; align-items: center; color: rgb(53, 50, 57);"] > img').click() // SourceofIncome
+        cy.wait(1000)
+        cy.get(`.scroll-content > :nth-child(${Math.floor(Math.random() * 250) + 1}`).click()
         cy.wait(700)
+        cy.get(':nth-child(5) > [style="display: flex; flex-direction: column;"] > .textfield-input').clear().type(`${rand}00`)
+        cy.get('.btn-confirm').scrollIntoView({ duration: 500 }).click()
+
+        cy.get('.title').should('have.text', 'Contact information')
         cy.get('.btn-confirm').click()
+
+        cy.get('.title').should('have.text', 'Review information')
+        cy.get('.btn-confirm').scrollIntoView({ duration: 500 }).click()
         cy.wait(1000)
         cy.get(':nth-child(2) > span').click()
       }
--- Fatca-Webview-UAT.cy.js
+++ Fatca-Webview-UAT.cy.js
@@ -1,69 +1,64 @@
-describe ('Fatca have Tin',() =>{
-    let random = Math.floor(Math.random() * 250)+1;
-    let random2 = Math.floor(Math.random() * 250)+1;
-    let extractedUrl;
-    let Url;
-    it('Visit',() => {
+describe('Fatca have Tin', () => {
 
-        cy.request({
-            method: 'GET',
-            form: true,
-            url: 'https://api-sit.partners.scb/partners/v3/consent/3f3e065b-279f-459c-8395-12707aa7ab36',
-            headers: {
-              'Cookie':'TS014b3884=01bfa4d02968039667d03c13ec098345b79491195120db61635806ae2e336bb89fdd8a987f8dec47018d0f5d5ca9cd814770926ef5',
-              'Cookie':'TS014b3884=01bfa4d0294ba018919fb22ac88d0544bbee5046bf36a9d9a5b719b1d5f9e4ad73f199b417547f603b2232c6ba0e1e2d5d33923117',
-              'apikey':'l7bfcf066a5d0d4ff6b48657cb57f5e0f9',
-              'apisecret':'5474e01e90eb44e6963c5c2191d5a83e',
-              'content-type':'application/json',
-              'accept-language':'en',
-              'resourceOwnerId':'00000023722490',
-              'requestUId':'5c00d79e-2cca-4413-b40a-d6ab8e9eddcf'   
-            },
-            
-          }).then(response => {
-            // ตรวจสอบว่า response.status เท่ากับ 200 และ response.data.data.callbackUrl มีอยู่
-            if (response.status === 200 && response.body.status.code === 1000) {
-              const Url = response.body.data.callbackUrl;
-              console.log(Url);
-              extractedUrl = Url.split('=')[1];
-              console.log(extractedUrl);
-              cy.log('Check Code')
-              expect(extractedUrl).to.not.be.undefined; // ตรวจสอบว่าค่า extractedUrl ไม่ใช่ undefined
+  let rm14 = "00000023884901";
+
+  let random = Math.floor(Math.random() * 250) + 1;
+  let random2 = Math.floor(Math.random() * 250) + 1;
+  let extractedUrl;
+  let Url;
+  it('Visit', () => {
+
+    cy.request({
+      method: 'GET',
+      url: 'https://api-uat.partners.scb/partners/v3/consent/ddb3107b-5f89-49d7-954e-d52ecaa49a38',
+      headers: {
+        'Cookie': 'TS019cb087=01bfa4d029df49db2ec51bc064e170eea9252a9c0e5b20c1fed6b0f46661ca2790ab5f68a1b4f9fd62a8ffea2aa179d7dcd62b6de2',
+        'apikey': 'l7311f7148299e4504aa4eba4a7536f5a9',
+        'apisecret': '3dc5032451bc446cbc574aec25d01247',
+        'content-type': 'application/json',
+        'accept-language': 'en',
+        'resourceOwnerId': rm14,
+        'requestUId': '5c00d79e-2cca-4413-b40a-d6ab8e9eddcf'
+      },
+
+    }).then(response => {
+      // ตรวจสอบว่า response.status เท่ากับ 200 และ response.data.data.callbackUrl มีอยู่
+      if (response.status === 200 && response.body.status.code === 1000) {
+        const Url = response.body.data.callbackUrl;
+        console.log(Url);
+        extractedUrl = Url.split('=')[1];
+        console.log(extractedUrl);
+        cy.log('Check Code')
+        expect(extractedUrl).to.not.be.undefined; // ตรวจสอบว่าค่า extractedUrl ไม่ใช่ undefined
 
         cy.log('OpenURL');
-        cy.visit(`https://fatcaportal-sit.se.scb.co.th/crs/web?ekycMethod=default&accept-language=TH&referenceID=${extractedUrl}&userType=ETB&UserId=ENET&funcNm=Search&service=crs`);
+        cy.visit(`https://fatcaportal-uat.se.scb.co.th/crs/web?ekycMethod=default&accept-language=TH&referenceID=${extractedUrl}&userType=ETB&UserId=ENET&funcNm=Search&service=crs`);
         cy.wait(4000)
 
         cy.log('Answer Fatca');
         cy.get('#ansNo_0').click();
-        cy.wait(1000)
         cy.get('#ansNo_1').click();
-        cy.wait(1000)
         cy.get('#ansNo_2').click();
-        cy.wait(1000)
         cy.get('#ansNo_3').click();
-        cy.wait(1000)
         cy.get('#ansNo_4').click();
-        cy.wait(1000)
         cy.get('#ansNo_5').click();
-        cy.wait(1000)
 
         cy.log('Confirm Fatca');
-        cy.get('.btn',{ timeout: 4000 }).debug().should('be.visible').click();
+        cy.get('.btn > .flex', { timeout: 4000 }).debug().should('be.visible').click();
         cy.wait(1000);
 
         cy.log('HaveTin');
         cy.get('#ansYes_0').click();
-        cy.get('.btn',{ timeout: 4000 }).debug().should('be.visible').click();
+        cy.get('.btn', { timeout: 4000 }).debug().should('be.visible').click();
 
         cy.log('AddTinNoreason');
-        cy.get(':nth-child(1) > .grid > .flex-row',{ timeout: 4000 }).debug().should('be.visible').click();
+        cy.get(':nth-child(1) > .grid > .flex-row', { timeout: 4000 }).debug().should('be.visible').click();
         cy.wait(1000)
-        cy.get(`:nth-child(${random})> .searchList__item`).scrollIntoView({ duration: 4000 }).click();
+        cy.get(`:nth-child(${random})> .searchList__item`).scrollIntoView({ duration: 1000 }).click();
         cy.wait(1000)
         cy.get(':nth-child(2) > .input-text').clear().type('FR3764324348');
         cy.wait(1000)
-        cy.get('.btn > .flex',{ timeout: 4000 }).debug().should('be.visible').click();
+        cy.get('.btn > .flex', { timeout: 4000 }).debug().should('be.visible').click();
 
         cy.log('Confirm tin');
         cy.get('.bg-\\[\\#7631C1\\]').debug().should('be.visible').click();
@@ -74,17 +69,17 @@ describe ('Fatca have Tin',() =>{
         cy.get(':nth-child(2) > .input-text').clear().type('Bankok');
 
         cy.log('Complete CRS');
-        cy.get('.btn',{ timeout: 4000 }).debug().should('be.visible').click();
+        cy.get('.btn', { timeout: 4000 }).debug().should('be.visible').click();
         cy.get('#vehicle1').scrollIntoView({ duration: 1000 }).click();
-        cy.get('.btn',{ timeout: 4000 }).debug().should('be.visible').click();
+        cy.get('.btn', { timeout: 4000 }).debug().should('be.visible').click();
         cy.log('test commit');
-                
 
-    } else {
+
+      } else {
         throw new Error('Response status is not as expected');
       }
 
     });
-    
-    });
+
+  });
 });
